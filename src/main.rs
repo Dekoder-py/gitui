c90ec8bd9b6bec91916d3579780919c4636c1271
Dekoder-py <kyle@codingcorner.dev>
2026-05-20 21:48:10 +1200

    get git log

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,20 @@ fn render(frame: &mut Frame) {
         .constraints(vec![Constraint::Percentage(40), Constraint::Percentage(50)])
         .split(frame.area());
 
-    frame.render_widget("Left side | log", layout[0]);
+    frame.render_widget(git_log().expect("error git"), layout[0]);
     frame.render_widget("Right side | diff", layout[1]);
+}
+
+fn git_log() -> Result<String, std::io::Error> {
+    use std::process::Command;
+
+    let output = Command::new("git")
+        .args(["log", "--oneline", "--graph"])
+        .output()?;
+
+    if !output.status.success() {
+        return Ok(String::from_utf8_lossy(&output.stderr).to_string());
+    }
+
+    Ok(String::from_utf8_lossy(&output.stdout).to_string())
 }
